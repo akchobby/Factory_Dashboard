375729356eccd203c26ac783f9a74ef527dbd84a
vish-96 <visvishal1996@gmail.com>
2019-10-18 11:38:32 +0200

    gui first change

--- src/dashboard/Gui.java
+++ src/dashboard/Gui.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class Gui {
 
@@ -39,12 +40,29 @@ public class Gui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 998, 668);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnThisIsA = new JButton("This is a button");
-		btnThisIsA.setBounds(151, 188, 153, 29);
-		frame.getContentPane().add(btnThisIsA);
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 0, 153, 375);
+		frame.getContentPane().add(panel);
+		
+		JTextPane txtpnAvailableFactory = new JTextPane();
+		txtpnAvailableFactory.setText("Available Factories");
+		panel.add(txtpnAvailableFactory);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(163, 0, 788, 375);
+		frame.getContentPane().add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(10, 374, 473, 217);
+		frame.getContentPane().add(panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(483, 374, 468, 217);
+		frame.getContentPane().add(panel_3);
 	}
 }
